3c2793dd0f61b926d1368ec456c829c8dda21e7d
Amr <amr.ouf96@gmail.com>
2020-05-26 11:38:51 +0200

    💄 UI: Responsive

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -58,8 +58,8 @@ const Profile = ({
       <Navbar history={history}></Navbar>
       <Container className={classes.pt}>
         <Grid container spacing={3}>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={3}></Grid>
+          <Grid item xs={12} sm={6}>
             <ProfileHeader
               viewedId={id}
               viewedUser={viewedUser}
